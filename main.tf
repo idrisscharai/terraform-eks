ab5b99ab3b3db59612223ce5b890d5cb4ee2cef9
Janis Rancans <rancans@gmail.com>
2020-12-08 23:51:40 +0200

    Helm alb ingress

--- main.tf
+++ main.tf
@@ -96,6 +96,16 @@ module "eks" {
       asg_desired_capacity          = 1
     },
   ]
+
+  workers_additional_policies = [aws_iam_policy.worker_policy.arn]
+
+}
+
+resource "aws_iam_policy" "worker_policy" {
+  name        = "worker-policy"
+  description = "Worker policy for the ALB Ingress"
+
+  policy = file("iam-policy.json")
 }
 
 ###############################################################################
@@ -173,6 +183,25 @@ data "helm_repository" "stable" {
   url  = "https://kubernetes-charts.storage.googleapis.com"
 }
 
+resource "helm_release" "ingress" {
+  name       = "ingress"
+  chart      = "aws-alb-ingress-controller"
+  repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
+  version    = "1.0.2"
+
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
+  }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  # set {
+  #   name  = "clusterName"
+  #   value = local.cluster_name
+  # }
+}
 
 ###############################################################################
 #################################### TODO #####################################
