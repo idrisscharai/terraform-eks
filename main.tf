cb81a98dd3ccd9aa11754f8edd76fdeacf039ff9
Janis Rancans <rancans@gmail.com>
2020-12-09 15:43:57 +0200

    fixed load balancer

--- main.tf
+++ main.tf
@@ -47,13 +47,27 @@ module "vpc" {
 
   cidr            = "10.0.0.0/16"
   azs             = data.aws_availability_zones.available.names
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.6.0/24"]
 
   enable_dns_hostnames = true
   enable_dns_support   = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
 
 }
 
@@ -168,30 +182,30 @@ provider "helm" {
   }
 }
 
-data "helm_repository" "stable" {
-  name = "stable"
-  url  = "https://kubernetes-charts.storage.googleapis.com"
-}
-
-resource "helm_release" "ingress" {
-  name       = "ingress"
-  chart      = "aws-alb-ingress-controller"
-  repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
-  version    = "1.0.2"
-
-  set {
-    name  = "autoDiscoverAwsRegion"
-    value = "true"
-  }
-  set {
-    name  = "autoDiscoverAwsVpcID"
-    value = "true"
-  }
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-}
+# data "helm_repository" "stable" {
+#   name = "stable"
+#   url  = "https://kubernetes-charts.storage.googleapis.com"
+# }
+#
+# resource "helm_release" "ingress" {
+#   name       = "ingress"
+#   chart      = "aws-alb-ingress-controller"
+#   repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
+#   version    = "1.0.2"
+#
+#   set {
+#     name  = "autoDiscoverAwsRegion"
+#     value = "true"
+#   }
+#   set {
+#     name  = "autoDiscoverAwsVpcID"
+#     value = "true"
+#   }
+#   set {
+#     name  = "clusterName"
+#     value = var.cluster_name
+#   }
+# }
 
 ###############################################################################
 #################################### TODO #####################################
