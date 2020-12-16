10b229cd9fd801e33dd06b84b4afa43136ec88cf
idrisscharai <56072732+idrisscharai@users.noreply.github.com>
2020-12-16 18:17:41 +0200

    fixed interpolation issue

--- main.tf
+++ main.tf
@@ -221,14 +221,14 @@ data "archive_file" "lambda_function" {
     
 resource "aws_lambda_function" "test_lambda" {
   
-  filename      = "${data.archive_file.lambda_function.output_path}"
+  filename      = data.archive_file.lambda_function.output_path
   function_name = "CloudWatch logs to S3 duplication"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   
   # This is to allow redeployment on AWS when the code changes because the hash changes as well.
   # Without this, even if the code changes, the source file doesn't change so there is no redeployment happening with terraform apply.
-  source_code_hash = filebase64sha256("${data.archive_file.lambda_function.output_path}")
+  source_code_hash = filebase64sha256(data.archive_file.lambda_function.output_path)
   
   runtime = "python3.6"
   
