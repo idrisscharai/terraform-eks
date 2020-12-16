e3165b7cf3b7c8ec2dcac5cc3e44aa2868f781cb
idrisscharai <56072732+idrisscharai@users.noreply.github.com>
2020-12-16 18:50:41 +0200

    fixed lambda function??!!!

--- main.tf
+++ main.tf
@@ -222,7 +222,7 @@ data "archive_file" "lambda_function" {
 resource "aws_lambda_function" "test_lambda" {
   
   filename      = data.archive_file.lambda_function.output_path
-  function_name = "CloudWatch logs to S3 duplication"
+  function_name = "test_lambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   
